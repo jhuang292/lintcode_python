046533dffcce4977ecc8f8a4ea5be0794c28f432
jhuang292 <jhuang292@wisc.edu>
2019-01-09 23:29:30 -0600

    classical binary search, note that the condition in the while loop

---
+++ 458_last_position_of_target.py
@@ -0,0 +1,28 @@
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def lastPosition(self, nums, target):
+        # write your code here
+        if (len(nums) == 0):
+            return -1
+        
+        start, end = 0, len(nums)-1
+        while (start + 1 < end):
+            mid = start + (end - start) // 2
+            
+            if (nums[mid] > target):
+                end = mid
+            elif (nums[start] < target):
+                start = mid
+            else:
+                start = mid
+                
+        if (nums[end] == target):
+            return end
+        if (nums[start] == target):
+            return start
+            
+        return -1
