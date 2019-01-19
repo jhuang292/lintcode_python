2e958ca5bc9480527c97b7efc20f05ec0d14a00e
jhuang292 <jhuang292@wisc.edu>
2019-01-19 17:56:02 -0600

    two sum III

---
+++ 607_Two_Sum_III.py
@@ -0,0 +1,25 @@
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):
+        self.count = {}
+    
+    def add(self, number):
+        # write your code here
+        if number in self.count:
+            self.count[number] += 1 
+        else:
+            self.count[number] = 1 
+        
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        # write your code here
+        for num in self.count:
+            if value - num in self.count and (value - num != num or self.count[num] > 1):
+                return True
+        return False
