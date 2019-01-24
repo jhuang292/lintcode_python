00742e5ceea0d784ddc0861583c59596f1b6aecd
jhuang292 <jhuang292@wisc.edu>
2019-01-24 13:14:45 -0600

    partition array

---
+++ 31_partition_array.py
@@ -0,0 +1,19 @@
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        start, end = 0, len(nums) - 1 
+        while start <= end:
+            while start <= end and nums[start] < k:
+                start += 1 
+            while start <= end and nums[end] >= k:
+                end -= 1 
+            if start <= end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1 
+                end -= 1 
+        return start
